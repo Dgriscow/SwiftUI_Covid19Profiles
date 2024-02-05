4688f20fdb533f15a98f124d5ae71ba3af2a92e4
devingrischow <devingrischow@outlook.com>
2024-02-05 12:37:00 -0600

    Initial Commit

---
+++ SwiftUI_Covid19Profiles/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI_Covid19Profiles
+//
+//  Created by Devin Grischow on 2/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI_Covid19Profiles/SwiftUI_Covid19ProfilesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Covid19ProfilesApp.swift
+//  SwiftUI_Covid19Profiles
+//
+//  Created by Devin Grischow on 2/5/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Covid19ProfilesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
